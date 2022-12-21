3518dc6aed8ab36390c8ab296bce4dc7bd4f3ecf
LevitD <diml71@mail.ru>
2022-12-21 19:56:31 +0300

    create metod

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+string[] smolArray(string[] inArray)
+{
+    int countSmolString = 0;
+    foreach (string item in inArray)
+    {
+        countSmolString += (item.Length <= 3) ? 1 : 0;
+    }
+    string[] newArray = new string[countSmolString];
+    return newArray;
+}
+
+//string[] array1 = new string[] {"sfgdb sdfgb", "fr", "advasdf", "hjjhgbkh"};
+
+WriteLine($"[{String.Join(",", smolArray(new string[] { "sfgdb sdfgb", "fr", "advasdf", "hjjhgbkh" }))}]");
