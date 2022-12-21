493ddf6c25f793d7d7a6d9ee71144f5b5d457f14
LevitD <diml71@mail.ru>
2022-12-21 21:38:50 +0300

    add block diagram

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@ using static System.Console;
 
 Clear();
 
-string[] smolArray(string[] inArray)
+string[] smallArray(string[] inArray)
 {
-    int countSmolString = 0;
+    int countSmallString = 0;
     foreach (string item in inArray)
     {
-        countSmolString += (item.Length <= 3) ? 1 : 0;
+        countSmallString += (item.Length <= 3) ? 1 : 0;
     }
-    string[] newArray = new string[countSmolString];
+    string[] newArray = new string[countSmallString];
     int i = 0;
     foreach (string item in inArray)
     {
@@ -23,8 +23,6 @@ string[] smolArray(string[] inArray)
     return newArray;
 }
 
-
-
 string[] array1 = new string[] { "Программа", "выделяет", "массивы", "из", "3-х", "и менее", "символов" };
 string[] array2 = new string[] { "Hello", "2", "world", ":)" };
 string[] array3 = new string[] { "1234", "1567", "-2", "computer scince" };
@@ -34,7 +32,7 @@ object[] objectArrays = new object[] { array1, array2, array3, array4 };
 
 foreach (string[] item in objectArrays)
 {
-    WriteLine($"[{String.Join(", ", item )}] ->[{String.Join(", ", smolArray(item))}]"); //
+    WriteLine($"[{String.Join(", ", item )}] ->[{String.Join(", ", smallArray(item))}]"); //
 }
 
 
